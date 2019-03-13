a5b4d1234099ed4d8ac002fe3b100a1745e3b046
samir.zaarouri <samir.zaarouri@epita.fr>
2019-03-13 17:22:14 +0100

    soutenance 1
    menu carte et scene 1

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame() 
+    {
+        SceneManager.LoadScene("Map");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();    
+    }
+
+} 
